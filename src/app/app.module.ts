23e3e85f80076907bffd49414bbafaabc94f6a2c
Gyp02 <gh0enr@gmail.com>
2023-04-20 16:12:24 -0500

    se ajusto y re diseño la entrada, agregamoss libreria typed y implementamos, falta ajustar la aparicion sobre el componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { MisproductosComponent } from './componets/misproductos/misproductos.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxTypedJsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
